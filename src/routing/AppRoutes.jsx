64a41f59d70cfbfa6a858b42815508fb51942598
Piotr Jaczewski <pp.jaczewski@gmail.com>
2022-02-07 11:00:55 +0100

    gh routing fix

--- src/routing/AppRoutes.jsx
+++ src/routing/AppRoutes.jsx
@@ -9,10 +9,11 @@ import { AppRoute } from './AppRoute.enum';
 export const AppRoutes = () => {
   return (
     <Switch>
-      <Route path={AppRoute.home} exact component={Products} />
+      {/* <Route path={AppRoute.home} exact component={Products} /> */}
+      <Route path={process.env.PUBLIC_URL + '/'} exact component={Products} />
       <Route path={AppRoute.login} component={Login} />
 
-      <Redirect to={AppRoute.home} exact component={Products} />
+      <Redirect to={process.env.PUBLIC_URL + '/'} />
     </Switch>
   );
 };
